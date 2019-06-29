e75646372c3afe7c3da26490e5f2622cbb31a873
Adam Chudas <achudas@mimiker.ii.uni.wroc.pl>
2019-06-29 07:48:46 +0200

    Do not proceed with a killpg test in the process group of the Init process.

--- bin/utest/pgrp.c
+++ bin/utest/pgrp.c
@@ -68,65 +68,69 @@ int test_kill(void) {
   return 0;
 }
 
-/* In this tests there are five processes marked with: init, a, b, c and d.
- * Process groups are marked with brackets: (init -> a) --> (b -> c) --> (d)
- * Process d sends signal to the process group containing b and c.
- * Process a sends signal to own process group containing init and a. */
+/* In this tests there are five processes marked with: a, b, c, d, e.
+ * Process groups are marked with brackets: (a -> b) --> (c -> d) --> (e)
+ * Process e sends signal to the process group containing c and d.
+ * Process b sends signal to own process group containing a and b. */
 int test_killpg(void) {
   kill_tests_setup();
 
   pid_t pid_a = fork();
   if (pid_a == 0) {
+    setpgid(0, 0);
 
     pid_t pid_b = fork();
     if (pid_b == 0) {
-      setpgid(0, 0);
-      pid_t pgid_b = getpgid(0);
 
       pid_t pid_c = fork();
       if (pid_c == 0) {
+        setpgid(0, 0);
+        pid_t pgid_c = getpgid(0);
 
         pid_t pid_d = fork();
         if (pid_d == 0) {
-          setpgid(0, 0);
 
-          assert(!killpg(pgid_b, SIGUSR1));
-          /* Process d should not receive
-           * signal from process d. */
-          assert(!sig_delivered);
+          pid_t pid_e = fork();
+          if (pid_e == 0) {
+            setpgid(0, 0);
+
+            assert(!killpg(pgid_c, SIGUSR1));
+            /* Process e should not receive
+             * signal from process e. */
+            assert(!sig_delivered);
+            exit(0); // process e
+          }
+
+          wait(NULL);
+          /* Process d should receive signal from process e. */
+          assert(sig_delivered);
           exit(0); // process d
         }
 
         wait(NULL);
-        /* Process c should receive signal from process d. */
+        /* Process c should receive signal from process e. */
         assert(sig_delivered);
         exit(0); // process c
       }
-
       wait(NULL);
-      /* Process b should receive signal from process d. */
+      /* Process b should not receive signal from process e. */
+      assert(!sig_delivered);
+      assert(!killpg(0, SIGUSR1));
+      /* Process b should receive signal from process b. */
       assert(sig_delivered);
       exit(0); // process b
     }
 
     wait(NULL);
-    /* Process a should not receive signal from process d. */
-    assert(!sig_delivered);
-    assert(!killpg(0, SIGUSR1));
-    /* Process a should receive signal from process a. */
+    /* Process a should receive signal from process b. */
     assert(sig_delivered);
-    exit(0); // process a
+    /* It is forbidden to send signal to non-existing group. */
+    assert(killpg(pid_b, SIGUSR1));
+    /* Invalid argument. */
+    assert(killpg(1, SIGUSR1));
+    /* Invalid argument (negative number). */
+    assert(killpg(-1, SIGUSR1));
   }
-
-  wait(NULL);
-  /* Process init should receive signal from process a. */
-  assert(sig_delivered);
-  /* It is forbidden to send signal to non-existing group. */
-  assert(killpg(2, SIGUSR1));
-  /* Invalid argument. */
-  assert(killpg(1, SIGUSR1));
-  /* Invalid argument (negative number). */
-  assert(killpg(-1, SIGUSR1));
 
   return 0;
 }
